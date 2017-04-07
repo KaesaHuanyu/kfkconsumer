b183b3ea0b50920c15685c92339cefa27834e72c
黎寰宇 <kaesalai@gmail.com>
2017-04-07 18:28:52 +0800

    fix

---
+++ src/kafkaconsumer/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/Shopify/sarama"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+
+	consumer, err := sarama.NewConsumer([]string{"broker01:9092", "broker02:9092", "broker03:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := consumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	partitionConsumer, err := consumer.ConsumePartition("my_topi", 0, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := partitionConsumer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// Trap SIGINT to trigger a shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	consumed := 0
+ConsumerLoop:
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			log.Printf("Consumed message offset %d\n", msg.Offset)
+			consumed++
+		case <-signals:
+			break ConsumerLoop
+		}
+	}
+
+	log.Printf("Consumed: %d\n", consumed)
+}
